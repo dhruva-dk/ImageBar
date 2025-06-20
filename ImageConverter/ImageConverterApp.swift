f69b0eb5a088d2e7a16c3fb33b421171424eaeeb
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2025-06-20 10:44:44 -0500

    Initial Commit

---
+++ ImageConverter/ImageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageConverterApp.swift
+//  ImageConverter
+//
+//  Created by Dhruva Kumar on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
