2112f610c9fcce51a72c5f99a0ae079405018ae8
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2025-06-21 12:59:00 -0500

    comm it b4 testing dragging

--- ImageConverter/ImageConverterApp.swift
+++ ImageConverter/ImageConverterApp.swift
@@ -1,15 +1,22 @@
 import SwiftUI
+import MenuBarExtraAccess
 
 
 @main
 struct ImageConverterApp: App {
     @StateObject private var settingsViewModel = SettingsViewModel()
+    @State var isMenuPresented: Bool = false
     
 
     var body: some Scene {
         MenuBarExtra("Image Converter", systemImage: "photo.on.rectangle") {
             AppMenu()
                 .environmentObject(settingsViewModel)
+
+        }
+
+        .menuBarExtraAccess(isPresented: $isMenuPresented) { statusItem in // <-- the magic ✨
+             // access status item or store it in a @State var
         }
         
         Settings {
